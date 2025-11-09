cd29e2879dc04b9d4b49aaebcf48652ba938ca7a
Ducks <cansgamer@gmail.com>
2025-11-09 12:00:00 -0500

    post

---
+++ main.js
@@ -0,0 +1,39 @@
+import { posts } from "./posts.js";
+
+const all_posts = document.getElementById("all_posts");
+const all_posts_category = document.getElementById("all_posts_category");
+
+function init() {
+    let ret = "";
+
+    cant_post.innerHTML = `(${posts.length})`;
+
+    for (const key of posts.slice(0, 5)) {
+        ret += `
+            <li>${key.fecha} <a href="${key.url_post}">${key.title}</a></li>
+        `;
+
+    }
+    all_posts.innerHTML = ret;
+
+    const categorias = {};
+
+    for (const post of posts) {
+        if (!post.category) continue;
+        if (!categorias[post.category]) {
+            categorias[post.category] = [];
+        }
+        categorias[post.category].push(post);
+    }
+
+    let retCat = "";
+    for (const categoria in categorias) {
+        retCat += `
+            <li><a href="post_category.html?cat=${categoria}">${categoria}</a></li>
+        `
+    }
+
+    all_posts_category.innerHTML = retCat;
+}
+
+window.onload = init;
---
+++ posts.js
@@ -0,0 +1,9 @@
+export const posts = [
+    {
+        id: "1",
+        title: "Testing",
+        category: "",
+        url_post: "posts/test.html",
+        fecha: "2025-11-09",
+    }
+];
